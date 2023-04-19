8b413edb033a009273bf532aa9670e301bacc2d0
Dev Vaibhav <vaibhav.d@northeastern.edu>
2023-04-19 15:18:59 -0400

    sample structure

---
+++ include/helper_functions.h
@@ -0,0 +1,13 @@
+/*Dev Vaibhav, Siddharth Maheshwari, Skanda Akkihebbal Prasanna
+Spring 2023 CS 5330
+Final Project: Autonomous Lane and Number plate detection using classic Computer Vision and Deep Learning 
+*/
+
+
+// Declaring global variables example
+
+extern cv::Size patternsize; //interior number of corners
+// Resolution required for the window
+extern int res_width; //columns
+extern int res_height; //rows
+
---
+++ src/helper_functions.cpp
@@ -0,0 +1,5 @@
+/*Dev Vaibhav, Siddharth Maheshwari, Skanda Akkihebbal Prasanna
+Spring 2023 CS 5330
+Final Project: Autonomous Lane and Number plate detection using classic Computer Vision and Deep Learning 
+*/
+
