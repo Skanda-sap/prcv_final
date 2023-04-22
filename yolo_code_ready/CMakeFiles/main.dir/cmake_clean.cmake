354d95544bdfc88aee611f59703c439d0a8a42f2
Dev Vaibhav <vaibhav.d@northeastern.edu>
2023-04-21 23:08:35 -0400

    num plate detection done

---
+++ yolo_code_ready/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/yolov5.cpp.o"
+  "CMakeFiles/main.dir/yolov5.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
